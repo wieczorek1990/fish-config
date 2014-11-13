165fb010a7c12a3957928ea11025ba25a4d24a47
Łukasz Wieczorek <wieczorek1990@gmail.com>
2014-11-13 12:43:43 +0100

    fixes

--- config.fish
+++ config.fish
@@ -4,9 +4,11 @@ set -x EDITOR vi
 set -x NVM_DIR ~/.nvm
 set -x JAVA_HOME /usr/lib/jvm/jdk1.9.0
 # Waiting for rvm team to fix PATH issue
-set PATH ~/.linuxbrew/bin ~/.nvm /opt/android-sdk-linux/tools $PATH
+set PATH ~/.linuxbrew/bin ~/.nvm ~/.nvm/current/bin /opt/libwebp/bin /opt/android-sdk-linux/tools $PATH
 for cmd in download purge search show simi simp
   complete -c $cmd -a '(__fish_print_packages)'
 end
 rvm default
 
+
+test -s /home/luke/.nvm-fish/nvm.fish; and source /home/luke/.nvm-fish/nvm.fish
