5550631acdfab92ea4d3556b4d2cd0ea451958a5
Łukasz Wieczorek <wieczorek1990@gmail.com>
2014-11-30 00:11:47 +0100

    vim pager; completion fix

--- config.fish
+++ config.fish
@@ -4,7 +4,8 @@ set fish_function_path \
 set fish_greeting
 set -x EDITOR vi
 set -x JAVA_HOME /usr/lib/jvm/jdk1.8.0_25
-for cmd in download purge search show simi simp
+set -x PAGER "/bin/sh -c \"col -b -x | vim - -c 'set ft=man nomod nonumber readonly' -c 'map q :q!<CR>'\""
+for cmd in download purge search show simi simp in unin
   complete -c $cmd -a '(__fish_print_packages)'
 end
 rvm default
