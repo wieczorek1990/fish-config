2c790be687c1aa77206dfd24331bf77db1a3c940
Łukasz Wieczorek <wieczorek1990@gmail.com>
2014-11-30 18:18:45 +0100

    removed git prompt due to use of gitsh

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,12 +1,7 @@
 function fish_prompt --description 'Write out the prompt'
-  set -l last_status $status
   set_color $fish_color_cwd
-  echo -n (prompt_pwd)
+  echo -n (whoami)' '(prompt_pwd)' '
   set_color normal
-  printf '%s ' (__informative_git_prompt)
-  if not test $last_status -eq 0
-    set_color $fish_color_error
-  end
   echo -n '$ '
   set_color $fish_color_normal
 end
