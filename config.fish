0044f6b7b0914086b3df57a3b113ce562c806b71
Łukasz Wieczorek <lukasz.wieczorek@unit9.com>
2014-12-07 12:34:46 +0100

    added user config

--- config.fish
+++ config.fish
@@ -14,4 +14,8 @@ set -x TERMINFO ~/.terminfo
 for cmd in download purge search show simi simp in unin
   complete -c $cmd -a '(__fish_print_packages)'
 end
+set user_config ~/.config/fish/user_config.fish
+if test -e $user_config
+  source $user_config
+end
 
