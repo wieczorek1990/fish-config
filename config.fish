873281ad84ef43a393acb42d3da2cb7c20eb9844
Łukasz Wieczorek <lukasz.wieczorek@unit9.com>
2015-02-09 18:08:21 +0100

    Added some libs to path

--- config.fish
+++ config.fish
@@ -3,11 +3,17 @@ set fish_function_path \
     $fish_function_path
 set fish_greeting
 set -x EDITOR vi
+set -x GOROOT /opt/go
 set -x JAVA_HOME /usr/lib/jvm/jdk1.8.0_25
 set -x PATH \
        $JAVA_HOME \
+       /opt/mongodb/bin \
        /opt/android-sdk/tools \
        /opt/android-sdk/platform-tools \
+       /opt/libwebp/bin \
+       /opt/go/bin \
+       /opt/go_appengine \
+       /opt/google_appengine \
        $PATH
 set -x TERM screen-256color
 set -x TERMINFO ~/.terminfo
