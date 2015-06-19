f04f74aaa0842c8516ad12a0e16951f02d0e2e57
Łukasz Wieczorek <wieczorek1990@gmail.com>
2015-06-19 15:25:52 +0200

    Updated config.

--- config.fish
+++ config.fish
@@ -7,19 +7,16 @@ set -x GOROOT /opt/go
 set -x JAVA_HOME /usr/lib/jvm/jdk1.8.0_25
 set -x PATH \
        $JAVA_HOME \
-       /opt/mongodb/bin \
-       /opt/android-sdk/tools \
-       /opt/android-sdk/platform-tools \
-       /opt/libwebp/bin \
-       /opt/go/bin \
-       /opt/go_appengine \
-       /opt/google_appengine \
+       ~/Software/android-sdk/tools
+       ~/Software/android-sdk/platform-tools
        $PATH
 set -x TERM screen-256color
 set -x TERMINFO ~/.terminfo
+
 for cmd in download purge search show simi simp in unin
   complete -c $cmd -a '(__fish_print_packages)'
 end
+
 set user_config ~/.config/fish/user_config.fish
 if test -e $user_config
   source $user_config
