c33ed66d1e161ca65b5d0504677762e5d10f2d5d
Łukasz Wieczorek <lukasz.wieczorek@unit9.com>
2015-06-24 11:28:01 +0200

    Updated email.

--- functions/gh.fish
+++ functions/gh.fish
@@ -1,5 +1,5 @@
 function gw
-  git config --global user.email "wieczorek1990@gmail.com"
+  git config --global user.email "luke.wieczorek@outlook.com"
   git config --global user.name "Łukasz Wieczorek"
 end
 
