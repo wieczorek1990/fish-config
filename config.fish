0334a10aefa5f67a4e806b5cad13c1561368d8ed
Łukasz Wieczorek <wieczorek1990@gmail.com>
2016-02-11 10:30:32 +0100

    Removed TERM settings

--- config.fish
+++ config.fish
@@ -3,8 +3,6 @@ set fish_function_path \
     $fish_function_path
 set fish_greeting
 set -x EDITOR vi
-set -x TERM screen-256color
-set -x TERMINFO ~/.terminfo
 
 for cmd in download purge search show simi simp in unin
   complete -c $cmd -a '(__fish_print_packages)'
