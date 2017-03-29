e94ab17ce9c67bea2216988f61115e17182a8ffc
Łukasz Wieczorek <lwieczorek@apptension.com>
2017-03-29 11:39:59 +0200

    Update dpurge

--- functions/dpurge.fish
+++ functions/dpurge.fish
@@ -1,8 +1,7 @@
-#!/bin/sh
-
 function dpurge
-	sudo docker stop (sudo docker ps -a -q)
-	sudo docker rm --force (sudo docker ps -a -q)
-	sudo docker rmi --force (sudo docker images -q
+  sudo docker stop (sudo docker ps -a -q)
+  sudo docker rm --force (sudo docker ps -a -q)
+  sudo docker rmi --force (sudo docker images -q)
+  sudo docker volume rm (sudo docker volume list | tr -s ' ' | cut -f2 -d' ')
 end
 
