7d8c9b49069ed7954fb04090667dcc951f50339f
Łukasz Wieczorek <wieczorek1990@gmail.com>
2017-12-16 11:22:54 +0100

    Remove python and ruby packages upgrade

--- functions/upgrade.fish
+++ functions/upgrade.fish
@@ -1,7 +1,4 @@
 function upgrade
   sudo dnf upgrade --refresh --best --allowerasing -y
-  sudo pip install -U (pip list --format=legacy | cut -f1 -d' ')
-  sudo gem update --system
-  sudo gem update
 end
 
